bda1a88556654af723e96560f3a31c0301f99ebe
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-06-05 01:18:40 +0530

    Learning about different datatypes

---
+++ Datatypes.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // int long float double 
+    // string and  getline
+    // char 
+
+    // by using Getline, it takes all the string present on the line 
+
+    string s;
+    cin >> s;
+    cout << s;
+
+    string s1;
+    getline(cin,s1);
+    cout << s1;
+
+    int a;
+    cin >> a;
+    cout << a;
+
+    float b;
+    cin >> b;
+    cout << b;
+
+    char c;
+    cin >> c;
+    cout << c;
+
+
+return 0;
+}
