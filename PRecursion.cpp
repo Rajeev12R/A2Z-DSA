62bbe6743e8f2c580cdf83f8abdc4e217bb00a1a
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-06-09 02:23:05 +0530

    Problems of Recursion

---
+++ PRecursion.cpp
@@ -0,0 +1,36 @@
+/*
+#include<bits/stdc++.h>
+using namespace std;
+ void solve(int n) {
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+      sum += i;
+    }
+    cout<<"The sum of the first "<<n<<" numbers is: "<<sum<<endl;
+  }
+ int main() {
+
+    solve(5);
+    solve(6);
+  }
+
+  
+
+#include<bits/stdc++.h>
+using namespace std;
+void fact(int m){
+  int mul = 1;
+  for(int i = 1; i <=m; i++){
+    mul *= i;
+  }
+  cout << "The factorial of the number "<<m<<" is: " << mul <<endl;
+}
+int main(){
+  int m;
+  cin >> m;
+  fact(m);
+
+return 0;
+}
+*/
+
