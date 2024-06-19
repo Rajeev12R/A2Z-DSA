fb3f2f6496e98112b46ecac54678577d0f8bed9e
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-06-19 22:23:06 +0530

    Initial commit

---
+++ temp.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int inc(int n)
+{
+    n++;
+    return n;
+}
+int main()
+{
+    int a = 3;
+    cout << a << endl;
+    a = inc(a);
+    cout << a << endl;
+}
